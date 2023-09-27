af4f6bb7e1b3d4de3883bbbcc76b890ad50f3a83
Baipzix <haitao2@ualberta.ca>
2023-09-27 10:53:27 -0700

    Add the work flow

---
+++ TensorFlow-WorkFlow.py
@@ -0,0 +1,23 @@
+#
+# Licensed under the Apache License, Version 2.0 
+#
+########################################################################
+
+########################################################################
+# 1. Import libraries and dataset 
+
+import tensorflow as tf
+import tensorflow_datasets as tfds 
+import numpy as np
+
+data = tfds.load("iris", split="train")
+
+
+########################################################################
+# 2. Data clean 
+
+ 
+########################################################################
+# 3. Transfor an normalize data 
+
+
